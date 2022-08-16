e8ade24a80626613e924887409971eb38d28ddc8
mohanthatha <98022082+mohanthatha@users.noreply.github.com>
2022-08-16 19:22:46 +0530

    Update SpringbootDemoApplication.java

--- src/main/java/net/javaguides/springboot/SpringbootDemoApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootDemoApplication.java
@@ -1,10 +1,19 @@
 package net.javaguides.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class SpringbootDemoApplication {
+	
+	@GetMapping("/hello")
+	public String getAll(){
+	return "helloworld";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
