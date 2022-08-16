b9b013d30b267f0f0f73e0492f8e2a70cdc61710
mohanthatha <98022082+mohanthatha@users.noreply.github.com>
2022-08-16 16:51:50 +0530

    Update NotesRepository.java

--- src/main/java/net/javaguides/springboot/NotesRepository.java
+++ src/main/java/net/javaguides/springboot/NotesRepository.java
@@ -1,11 +1,11 @@
 package net.javaguides.springboot;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 import net.javaguides.springboot.Notes.Notes;
 
 
-
+@Repository
 public interface NotesRepository extends CrudRepository<Notes,Integer>{
 
 }
